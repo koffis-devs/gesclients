dc375389a2d31cbd30994a930d02299b64454323
koffi stevens <koffistevens.g@gmail.com>
2021-06-01 14:19:46 +0000

    Creation du projet

---
+++ src/main/java/ci/koffis/gesclients/GesclientsApplication.java
@@ -0,0 +1,13 @@
+package ci.koffis.gesclients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesclientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesclientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ci/koffis/gesclients/GesclientsApplicationTests.java
@@ -0,0 +1,13 @@
+package ci.koffis.gesclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesclientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
